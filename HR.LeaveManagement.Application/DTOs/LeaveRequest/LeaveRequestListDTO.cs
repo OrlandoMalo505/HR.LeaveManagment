5ce6adf9e3d705b791fb9ca6e616e4c62ed1b43d
Orlando Malo <Orlando.Malo@fti.edu.al>
2022-08-24 23:38:51 +0200

    added automapper an dtos

---
+++ HR.LeaveManagement.Application/DTOs/LeaveRequest/LeaveRequestListDTO.cs
@@ -0,0 +1,17 @@
+﻿using HR.LeaveManagement.Application.DTOs.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Application.DTOs.LeaveRequest
+{
+    public class LeaveRequestListDTO : BaseDTO
+    {
+
+        public LeaveTypeDTO LeaveTypeDTO { get; set; }
+        public DateTime DateRequested { get; set; }
+        public bool? Approved { get; set; }
+    }
+}
