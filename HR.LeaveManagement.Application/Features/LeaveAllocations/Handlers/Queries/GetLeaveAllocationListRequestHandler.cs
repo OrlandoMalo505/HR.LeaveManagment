dbe2e6418d79a7d9192dd121ffd39b73095e131e
Orlando Malo <Orlando.Malo@fti.edu.al>
2022-08-29 23:56:30 +0200

    added request/handlers for leaveAllocation & leaveRequest

---
+++ HR.LeaveManagement.Application/Features/LeaveAllocations/Handlers/Queries/GetLeaveAllocationListRequestHandler.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.DTOs;
+using HR.LeaveManagement.Application.Features.LeaveAllocations.Requests.Queries;
+using HR.LeaveManagement.Application.Persistence.Contracts;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Application.Features.LeaveAllocations.Handlers.Queries
+{
+    public class GetLeaveAllocationListRequestHandler : IRequestHandler<GetLeaveAllocationListRequest, List<LeaveAllocationDTO>>
+    {
+        private readonly ILeaveAllocationRepository _leaveAllocationRepository;
+        private readonly IMapper _mapper;
+
+        public GetLeaveAllocationListRequestHandler(ILeaveAllocationRepository leaveAllocationRepository, IMapper mapper)
+        {
+            _leaveAllocationRepository = leaveAllocationRepository;
+            _mapper = mapper;
+        }
+        public async Task<List<LeaveAllocationDTO>> Handle(GetLeaveAllocationListRequest request, CancellationToken cancellationToken)
+        {
+            var leaveAllocationsList = await _leaveAllocationRepository.GetLeaveAllocationsWithDetails();
+            return _mapper.Map<List<LeaveAllocationDTO>>(leaveAllocationsList);
+        }
+    }
+}
