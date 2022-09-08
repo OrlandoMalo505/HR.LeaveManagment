1c91b2883ae5162dbf1606e6911198354a571bdd
Orlando Malo <Orlando.Malo@fti.edu.al>
2022-09-08 21:39:45 +0200

    added repository implementation

---
+++ HR.LeaveManagement.Persistence/PersistenceServicesRegistration.cs
@@ -0,0 +1,33 @@
+﻿using HR.LeaveManagement.Application.Persistence.Contracts;
+using HR.LeaveManagement.Persistence.Repositories;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Persistence
+{
+    public static class PersistenceServicesRegistration
+    {
+        public static IServiceCollection ConfigurePersistenceServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<LeaveManagementDbContext>(options =>
+
+                options.UseSqlServer(configuration.GetConnectionString("LMConnectionString"))
+            );
+
+            services.AddScoped(typeof(IGenericRepository<>), typeof(IGenericRepository<>));
+
+            services.AddScoped<ILeaveTypeRepository, LeaveTypeRepository>();
+            services.AddScoped<ILeaveRequestRepository, LeaveRequestRepository>();
+            services.AddScoped<ILeaveAllocationRepository, LeaveAllocationRepository>();
+
+            return services;
+        }
+    }
+}
