e97a9ec73f3454162bf936c9caa8efa8e979e47a
Orlando Malo <Orlando.Malo@fti.edu.al>
2022-09-09 23:31:12 +0200

    added controller for leavetype

---
+++ HR.LeaveManagement.Api/Controllers/LeaveTypesController.cs
@@ -0,0 +1,58 @@
+﻿using HR.LeaveManagement.Application.DTOs.LeaveType;
+using HR.LeaveManagement.Application.Features.LeaveTypes.Requests.Commands;
+using HR.LeaveManagement.Application.Features.LeaveTypes.Requests.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace HR.LeaveManagement.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LeaveTypesController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public LeaveTypesController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<LeaveTypeDTO>>> Get()
+        {
+            var leaveTypes = await _mediator.Send(new GetLeaveTypeListRequest());
+            return Ok(leaveTypes);  
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<LeaveTypeDTO>> Get(int id)
+        {
+            var leaveType = await _mediator.Send(new GetLeaveTypeDetailRequest { Id = id});
+            return Ok(leaveType);   
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] CreateLeaveTypeDTO createLeaveTypeDTO)
+        {
+            var command = new CreateLeaveTypeCommand { LeaveTypeDTO = createLeaveTypeDTO };
+            var response = await _mediator.Send(command);
+            return Ok(response);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> Put([FromBody] LeaveTypeDTO leaveTypeDTO)
+        {
+            var command = new UpdateLeaveTypeCommand { LeaveTypeDTO = leaveTypeDTO };
+            await _mediator.Send(command);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var command = new DeleteLeaveTypeCommand { Id = id };
+            await _mediator.Send(command);
+            return NoContent();
+        }
+    }
+}
