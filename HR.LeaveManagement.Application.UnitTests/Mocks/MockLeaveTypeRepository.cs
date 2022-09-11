895de55e08f020d89dcd18cb61607dd7ccedb1a4
Orlando Malo <Orlando.Malo@fti.edu.al>
2022-09-11 23:46:05 +0200

    added unit tests for leavetypes

---
+++ HR.LeaveManagement.Application.UnitTests/Mocks/MockLeaveTypeRepository.cs
@@ -0,0 +1,45 @@
+﻿using HR.LeaveManagement.Application.Contracts.Persistence;
+using HR.LeaveManagement.Domain;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Application.UnitTests.Mocks
+{
+    public static class MockLeaveTypeRepository
+    { 
+        public static Mock<ILeaveTypeRepository> GetLeaveTypeRepository()
+        {
+            var leaveTypes = new List<LeaveType>
+            {
+                new LeaveType
+                {
+                    Id = 1,
+                    Name = "Vacation Test",
+                    DefaultDays = 10
+                },
+                new LeaveType
+                {
+                    Id = 2,
+                    DefaultDays = 12,
+                    Name = "Sick Test"
+                }
+            };
+
+            var mockRepo = new Mock<ILeaveTypeRepository>();
+
+            mockRepo.Setup(r => r.GetAll()).ReturnsAsync(leaveTypes);
+
+            mockRepo.Setup(r => r.Add(It.IsAny<LeaveType>())).ReturnsAsync((LeaveType leaveType) =>
+            {
+                leaveTypes.Add(leaveType);
+                return leaveType;
+            });
+
+            return mockRepo;
+        }
+    }
+}
